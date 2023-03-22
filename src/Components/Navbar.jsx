32edc9727ea4d58c22f012e2536202a8a4b0bd6c
jrtaboraman <jr.taboraman@gmail.com>
2023-03-22 12:48:29 +0800

    header component completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,58 @@
 import React from "react";
 import "../Styles/Navbar.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillCartFill } from "react-icons/bs";
+import logo from "../assets/images/image_2023-03-21_145232999.png";
 
 const Navbar = () => {
   return (
     <>
       <div className="marucci">
-        <div>
-          <input />
+        <div className="search-bar">
+          <AiOutlineSearch className="search-bar__icon" />
+          <input
+            className="search-bar__input"
+            type="text"
+            placeholder="Search"
+          />
         </div>
-        <div>
-          <input />
+        <div className="header__logo">
+          <img src={logo} alt="Marucci Sports" />
         </div>
-        <div>
-          <input />
+        <div className="header__account-cart">
+          <span className="header__account">Account</span>
+          <span className="header__account">Cart</span>
+          <BsFillCartFill className="header__cart" />
         </div>
       </div>
-      <div className="navbar">
-        <a href="/">Bats</a>
-        <a href="/">Fielding Gloves</a>
-        <a href="/">Apparel</a>
-        <a href="/">Batting Gloves</a>
-        <a href="/">Gear</a>
-        <a href="/">Experience</a>
-        <a href="/">Teams</a>
-        <a href="/">Sale</a>
-      </div>
+      <nav className="menu">
+        <ul className="menu__list">
+          <li className="menu__item">
+            <a href="#">Bat</a>
+          </li>
+          <li className="menu__item">
+            <a href="#">Fielding Glove</a>
+          </li>
+          <li className="menu__item">
+            <a href="#">Apparel</a>
+          </li>
+          <li className="menu__item">
+            <a href="#">Batting Gloves</a>
+          </li>
+          <li className="menu__item">
+            <a href="#">Gear</a>
+          </li>
+          <li className="menu__item">
+            <a href="#">Experience</a>
+          </li>
+          <li className="menu__item">
+            <a href="#">Teams</a>
+          </li>
+          <li className="menu__item">
+            <a href="#">Sale</a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
